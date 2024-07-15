a27206cabda8ff9017f32acd093aec02d5ea3189
Miguel Á. Sastre <sastre113@gmail.com>
2024-07-15 20:48:57 +0200

    initial commit

---
+++ src/test/java/tdd/in/action/AppTest.java
@@ -0,0 +1,15 @@
+package tdd.in.action;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testBasic(){
+        Assertions.assertEquals(1,1);
+    }
+}

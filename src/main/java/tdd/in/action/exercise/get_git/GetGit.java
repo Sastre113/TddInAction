7b27f395f27a34821f1ec6ab3fa69e84ee6b04c7
Miguel Á. Sastre <sastre113@gmail.com>
2024-07-22 21:56:57 +0200

    Nuevo ejercicio. Crear un método que recupere los últimos commits de mi repo

---
+++ src/main/java/tdd/in/action/exercise/get_git/GetGit.java
@@ -0,0 +1,17 @@
+package tdd.in.action.exercise.get_git;
+
+/*
+ * Source --> https://retosdeprogramacion.com/ejercicios
+ * Crea un programa que lea los últimos 10 commits de este repositorio y muestre:
+ * - Hash
+ * - Autor
+ * - Mensaje
+ * - Fecha y hora
+ *
+ */
+public class GetGit {
+
+    public static void main(String[] args) {
+
+    }
+}
